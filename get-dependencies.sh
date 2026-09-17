6031e8f006f4a055f6ccc79a45e0a76af56ca1c8
Link4Electronics <link4electronics@gmail.com>
2026-09-17 18:34:34 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,24 +6,21 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-# pacman -Syu --noconfirm PACKAGESHERE
+pacman -Syu --noconfirm hicolor-icon-theme sdl2_mixer sdl2_ttf
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
-get-debloated-pkgs --add-common --prefer-nano
+get-debloated-pkgs --add-common --prefer-nano sdl2_image-mini
 
-# Comment this out if you need an AUR package
-#make-aur-package PACKAGENAME
+echo "Building Cataclysm-TLG..."
+echo "---------------------------------------------------------------"
+REPO="https://github.com/Cataclysm-TLG/Cataclysm-TLG"
+VERSION="$(git ls-remote "$REPO" HEAD | cut -c 1-9 | head -1)"
+git clone --depth 1 "$REPO" ./Cataclysm-TLG
+echo "$VERSION" > ~/version
 
-# If the application needs to be manually built that has to be done down here
-
-# if you also have to make nightly releases check for DEVEL_RELEASE = 1
-#
-# if [ "${DEVEL_RELEASE-}" = 1 ]; then
-# 	nightly build steps
-# else
-# 	regular build steps
-# fi
-
-# Note that when building manually, you want to output the version of the
-# application to a ~/version file and remove VERSION from make-appimage.sh
+cd ./Cataclysm-TLG
+export LDFLAGS="${LDFLAGS:-}" && export LDFLAGS="${LDFLAGS/-Wl,-z,pack-relative-relocs/}"
+export CXXFLAGS="${CXXFLAGS:-} -Wno-error=maybe-uninitialized -Wno-error=sfinae-incomplete"; for flag in "-Wp,-D_GLIBCXX_ASSERTIONS" "-fcf-protection" "-fstack-clash-protection"; do CXXFLAGS="${CXXFLAGS/$flag/}"; done
+make -j$(nproc) PREFIX=/usr PCH=0 RELEASE=1 USE_XDG_DIR=1 LTO=1 RUNTESTS=0 TESTS=0 LINTJSON=0 ASTYLE=0 LOCALIZE=1 LANGUAGES=all install
+make -j$(nproc) PREFIX=/usr PCH=0 RELEASE=1 USE_XDG_DIR=1 LTO=1 RUNTESTS=0 TESTS=0 LINTJSON=0 ASTYLE=0 LOCALIZE=1 LANGUAGES=all TILES=1 SOUND=1 install
