b8aa3bf3ada5485372ae02dfcf31dcf864d43acd
Link4Electronics <link4electronics@gmail.com>
2026-09-18 17:56:18 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -9,6 +9,7 @@ export ADD_HOOKS="self-updater.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export ICON=/usr/share/icons/hicolor/scalable/apps/com.cataclysm-tlg.cataclysm-tlg.svg
 export DESKTOP=/usr/share/applications/com.cataclysm-tlg.cataclysm-tlg.desktop
+export APPNAME=Cataclysm-TLG
 export STARTUPWMCLASS=cataclysm-tlg-tiles
 export DEPLOY_OPENGL=1
 
